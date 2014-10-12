68c6b580a9e6a9693f80e8505ad100985d0edbd4
Rich Allen <rallen@taoti.com>
2014-10-12 08:33:56 -0400

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,86 +8,45 @@ fi
 # Update homebrew recipes
 brew update
 
+#taps
+
+taps=(
+	homebrew/dupes
+	homebrew/apache
+)
+echo "installing binaries..."
+brew tap ${taps[@]}
+
 binaries=(
 	ansible
-	apple-gcc42
 	autoconf
-	automake
 	awscli
 	axel
 	bash
 	bash-completion
 	cheat
-	cloog
 	cmake
 	composer
 	coreutils
-	cowsay
-	drupal-code-sniffer
 	ec2-ami-tools
 	ec2-api-tools
-	flac
-	fortune
-	freeimage
-	freetype
-	gdbm
-	gettext
-	gfortran
 	git
-	git-crypt
-	gmp
-	htop-osx
-	icu4c
-	imagemagick
-	isl
 	jmeter
-	jp2a
-	jpeg
-	lame
-	less
-	libmpc
-	libogg
-	libpng
-	libsndfile
-	libtool
-	libvorbis
-	libyaml
 	mackup
 	mariadb
-	md5sha1sum
-	mongodb
-	mp3gain
-	mpfr
 	mtr
 	nmap
 	node
 	openssl
-	packer
 	patchutils
-	pcre
-	perceptualdiff
-	php-code-sniffer
-	php55
-	php55-apcu
-	php55-xdebug
+	php56
+	php56-apcu
+	php56-xdebug
 	phpunit
-	pkg-config
-	ponysay
-	ponysay-tool
-	ponythink
-	python3
 	rbenv
-	readline
-	ruby-build
-	scons
-	sqlite
 	ssh-copy-id
-	swig
-	unixodbc
 	unrar
 	wget
-	xz
-	zlib
 )
 
 echo "installing binaries..."
@@ -99,36 +58,34 @@ brew install caskroom/cask/brew-cask
 
 # Apps
 apps=(
-  alfred
+  	alfred
 	appcleaner
 	atom
 	bettertouchtool
 	chromecast
 	chrome-remote-desktop-host
-  dropbox
+  	dropbox
 	evernote
 	firefox
-  google-chrome
+  	google-chrome
 	google-drive
 	gpgtools
 	hipchat
 	phpstorm
-  spotify
-  skitch
+  	spotify
+  	skitch
 	skype
 	totalterminal
 	transmit
 	vagrant
 	virtualbox
-  vlc
+  	vlc
 )
 
 # Install apps to /Applications
 # Default is: /Users/$user/Applications
 echo "installing apps..."
 brew cask install --appdir="/Applications" ${apps[@]}
-
-brew cask alfred link
 
 brew tap caskroom/fonts
 
